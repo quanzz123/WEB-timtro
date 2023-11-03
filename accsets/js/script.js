46d7009b2bcc645847d6fef03922859c6cdd7d12
quanzz123 <nguyenquandc37@gmai.com>
2023-11-03 16:00:02 +0700

    page new

--- accsets/js/script.js
+++ accsets/js/script.js
@@ -11,3 +11,18 @@ window.addEventListener('scroll', function() {
 });
 
 
+// Lấy tất cả các biểu tượng trái tim theo class "heart-icon"
+const heartIcons = document.querySelectorAll(".heart-icon");
+
+// Thêm sự kiện click cho tất cả biểu tượng trái tim
+heartIcons.forEach((icon) => {
+  icon.addEventListener("click", function() {
+    if (this.style.color !== "rgba(255, 0, 0, 0.8)") {
+      this.style.color = "rgba(255, 0, 0, 0.8)"; // Nếu màu không phải đỏ, thay đổi màu thành đỏ
+    } else {
+      this.style.color = "rgba(221, 206, 206, 0.5)"; // Nếu màu đã là đỏ, đổi lại thành màu ban đầu
+    }
+  });
+});
+
+
