51aa1d6243b325d446040185289376302cc29576
quanzz123 <nguyenquandc37@gmai.com>
2023-11-16 15:13:50 +0700

    add

--- accsets/js/script.js
+++ accsets/js/script.js
@@ -76,3 +76,21 @@ function openTab(tabId) {
     // Thêm và loại bỏ lớp cho nút phân trang
     updatePagingButtons();
 }
+
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("myDropdown");
+    dropdownContent.classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};   
